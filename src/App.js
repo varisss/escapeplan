f4038d93873a32471d95c3918a22dfdae9d876d6
Varis Kitnarong <varis@Variss-MacBook-Air.local>
2021-10-12 17:55:35 +0700

    Add grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import { Grid } from './components/Grid';
+import logo from "./logo.svg";
+import "./App.css";
+import { Grid } from "./components/Grid";
+import jungle from "./images/jungle.jpeg";
+import { Timer } from "./components/Timer";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="game-title">Escape Plan</h1>
+    <div className='App' style={{ backgroundImage: `url(${jungle})` }}>
+      <h1 className='game-title'>Escape Plan</h1>
+      <div className='menu'>
+        <Timer />
+        <div className='mute' />
+      </div>
       <Grid />
     </div>
   );
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,7 +1,31 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from "react";
 
 export const Grid = () => {
-    return (
-        <div id="canvas" />
-    )
-}
+  const gridArray = [
+    [0, 0, 3, 0, 0],
+    [0, 1, 1, 1, 0],
+    [2, 0, 0, 1, 0],
+    [0, 1, 0, 1, 4],
+    [0, 0, 0, 1, 0],
+  ];
+  return (
+    <div id='grid'>
+      {gridArray.map((row) => {
+        return row.map((col) => {
+          switch (col) {
+            case 0:
+              return <div className='block free' />;
+            case 1:
+              return <div className='block obs' />;
+            case 2:
+              return <div className='block tunnel' />;
+            case 3:
+              return <div className='block warder' />;
+            case 4:
+              return <div className='block prisoner' />;
+          }
+        });
+      })}
+    </div>
+  );
+};
